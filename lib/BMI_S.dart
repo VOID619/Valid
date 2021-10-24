713b82462873eccfd047d1404351823f30d5ab98
Mohamed Ibrahim <92342092+VOID619@users.noreply.github.com>
2021-10-23 18:57:34 -0700

    Update BMI_S.dart

--- lib/BMI_S.dart
+++ lib/BMI_S.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:project12/modules/bmi_r/BMI_R.dart';
+
+import 'BMI_R.dart';
 class BMI extends StatefulWidget {
   @override
   _BMIState createState() => _BMIState();
@@ -11,162 +11,187 @@ class BMI extends StatefulWidget {
 class _BMIState extends State<BMI> {
   bool isMale=true;
   double height=100.0;
-  int age=10;
-  int weight=70;
-
-
+  int age=20;
+  int weight=75;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0,),),
+        title: Text('BMI CALCULATOR ',
+        style: TextStyle(
+          fontSize: 35.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),),
       ),
-      body:
-      Column(
-        children:
-        [
+      body:Column(
+        children: [
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(20.0),
               child: Row(
-                  children: [
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: ()
-                        {
-                          setState(() {
-                            isMale=true;
-                          });
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            color: isMale? Colors.blue:Colors.grey[400],
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.ac_unit),
-                              SizedBox(height: 10.0,),
-                              Text('MALE',style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold),),
+                children: [
+                 Expanded(
+                   child: GestureDetector(
+                     onTap: ()
+                     {
+                       setState(() {
+                         isMale=true;
+                       });
+                     },
+                     child: Container(
+                       child: Column(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: [
+                           Icon(Icons.ac_unit,),
+                           SizedBox(height: 10.0,),
+                           Text('MALE',
+                             style: TextStyle(
+                               fontWeight: FontWeight.bold,
+                               fontSize: 35.0,
+                               color: Colors.black,
+                             ),)
+                         ],
+                       ),
 
-                            ],
-                          ),
+                       decoration: BoxDecoration(
+                         color: isMale?Colors.blue:Colors.grey[400],
+                         borderRadius: BorderRadius.circular(20.0)
+                       ),
+                     ),
+                   ),
+                 ),
+                 SizedBox(width: 20.0,),
+                 Expanded(
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          isMale=false;
+                        });
+                      },
+                      child: Container(
+                       child: Column(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: [
+                           Icon(Icons.ac_unit,),
+                           SizedBox(height: 10.0,),
+                           Text('FEMALE',
+                             style: TextStyle(
+                               fontWeight: FontWeight.bold,
+                               fontSize: 35.0,
+                               color: Colors.black,
+                             ),)
+                         ],
+                       ),
+                        decoration: BoxDecoration(
+                            color: !isMale?Colors.blue:Colors.grey[400],
+                            borderRadius: BorderRadius.circular(20.0)
                         ),
-                      ),
+                 ),
                     ),
-                    SizedBox(width: 20.0,),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: ()
-                        {
-                          setState(() {
-                            isMale=false;
-                          });
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            color: !isMale ?Colors.blue:Colors.grey[400],
-                          ),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.ac_unit),
-                                SizedBox(height: 10.0,),
-                                Text('FEMALE',style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold),),
-                              ]),
-                        ),
-                      ),
-                    ),
-                  ]),
+                  ),
+                ],
+              ),
             ),
           ),
           Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.grey[400],
-                  ),
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'HEIGHT',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30.0),),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [
-                          Text(
-                            '${height.round()}',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 50.0),),
-                          Text(
-                            'cm',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),),
-
-
-                        ],),
-                      SizedBox(height: 10.0,),
-                      Slider(
-                        value: height,
-                        onChanged: (value){
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('HEIGHT',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                    SizedBox(height: 10.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: [
+                        Text('${height.round()}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 50.0,
+                        ),),
+                        Text('cm',
+                        style: TextStyle(
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold,
+                        ),)
+                      ],
+                    ),
+                    SizedBox(height: 20.0,),
+                    Slider(
+                      min: 100.0,
+                        max: 220.0,
+                        value:height ,
+                        onChanged:(value){
                           setState(() {
                             height=value;
                           });
-                          print(value.round());
-                        },
-                        min: 100.0,
-                        max: 220.0,
-                      ),
-                    ],
-                  ),
+                        }
+                    ),
+                  ],
                 ),
-              )
+              ),
+            ),
           ),
           Expanded(
-              child: Padding (
+            child:
+              Padding(
                 padding: EdgeInsets.all(20.0),
                 child: Row(
                   children: [
                     Expanded(
                       child: Container(
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
+                          borderRadius: BorderRadius.circular(20.0),
                           color: Colors.grey[400],
-
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text(
-                              'AGE',style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,),),
-                            Text(
-                              '${age}',style: TextStyle(fontSize: 40.0,fontWeight: FontWeight.bold,),),
+                            Text('AGE',
+                              style: TextStyle
+                                (
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 35.0),),
+                            SizedBox(height: 10.0,),
+                            Text('$age',
+                              style: TextStyle
+                                (
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 35.0),),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                //add
-                                FloatingActionButton(onPressed:(){
-                                  setState(() {
-                                    --age;
-                                  });
-                                },
-                                  mini :true,
-                                  child: Icon(Icons.remove),
-                                ),
+                                FloatingActionButton(
+                                    onPressed:(){
+                                      setState(() {
+                                        --age;
+                                      });
+                                    },
+                                  mini: true,
+                                child: Icon(Icons.remove),),
                                 SizedBox(width: 20.0,),
-                                FloatingActionButton(onPressed:(){
-                                  setState(() {
-                                    ++age;
-                                  });
-                                },
-                                  mini :true,
-                                  child: Icon(Icons.add),
-                                ),
+                                FloatingActionButton(
+                                  mini: true,
+                                  onPressed:(){
+                                    setState(() {
+                                      ++age;
+                                    });
+                                  },
+                                  child: Icon(Icons.add),),
                               ],
-                            )
+                            ),
                           ],
                         ),
                       ),
@@ -175,71 +200,77 @@ class _BMIState extends State<BMI> {
                     Expanded(
                       child: Container(
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
+                          borderRadius: BorderRadius.circular(20.0),
                           color: Colors.grey[400],
-
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
-
                           children: [
-                            Text(
-                              'WEIGHT',style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,),),
-                            Text(
-                              '${weight}',style: TextStyle(fontSize: 40.0,fontWeight: FontWeight.bold,),),
+                            Text('WEIGHT',
+                              style: TextStyle
+                                (
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 35.0),),
+                            SizedBox(height: 10.0,),
+                            Text('$weight',
+                              style: TextStyle
+                                (
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 35.0),),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                //add
-                                FloatingActionButton(onPressed:(){
-                                  setState(() {
-                                    --weight;
-                                  });
-                                },
-                                  mini :true,
-                                  child: Icon(Icons.remove),
-                                ),
+                                FloatingActionButton(
+                                  onPressed:(){
+                                    setState(() {
+                                      --weight;
+                                    });
+                                  },
+                                  mini: true,
+                                  child: Icon(Icons.remove),),
                                 SizedBox(width: 20.0,),
-                                FloatingActionButton(onPressed:(){
-                                  setState(() {
-                                    ++weight;
-                                  });
-                                },
-                                  mini :true,
-                                  child: Icon(Icons.add),
-                                ),
+                                FloatingActionButton(
+                                  mini: true,
+                                  onPressed:(){
+                                    setState(() {
+                                      ++weight;
+                                    });
+                                  },
+                                  child: Icon(Icons.add),),
                               ],
-                            )
+                            ),
                           ],
                         ),
                       ),
                     ),
                   ],
                 ),
-              )
+              ),
           ),
           Container(
-              color: Colors.blue,
-              width: double.infinity,
-              child: MaterialButton(
-                onPressed: ()
-                {
-                  double result =weight/pow(height/100,2);
-                  print(result.round());
-                  Navigator.push(context,MaterialPageRoute(builder:(context) =>BMIRE(
-                    age: age,
-                    isMale:isMale,
-                    result: result.round(),
-                  )
-                  ),
-                  );
-                },
-                height: 50.0,
-                child: Text('CALCULATE',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30.0,color: Colors.white),),
-              )
-          ),
-
-        ],),
+            width: double.infinity,
+            child: MaterialButton(onPressed: (){
+              double result =weight/pow(height/100,2);
+              print(result);
+              Navigator.push(context,MaterialPageRoute(builder: (context)=>BMI_R(
+                age: age,
+                result: result,
+                isMale:isMale,
+              ),),);
+            },
+            color: Colors.blue,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text('calculate',
+                style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,),),
+            ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
