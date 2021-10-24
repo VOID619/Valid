01159ac2bb1567e5bf143305dd642c155f541c9f
Mohamed Ibrahim <92342092+VOID619@users.noreply.github.com>
2021-10-23 18:57:00 -0700

    Update BMI_R.dart

--- lib/BMI_R.dart
+++ lib/BMI_R.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
-
-class BMIRE extends StatelessWidget {
+import 'BMI_R.dart';
+class BMI_R extends StatelessWidget {
   final int?age;
   final bool? isMale;
-  final int? result;
-  BMIRE({
-    @required this.age,
-    @required this.isMale,
-    @required this.result,
-  });
+  final double? result;
+  BMI_R({
+   @required  this.isMale ,
+   @required  this.result ,
+   @required  this.age ,
+});
+  double r(double result)
+  {
+    double re;
+    result=result*100;
+   re= result.round()/100;
+
+    return re;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI RESULT '),
-      ),
-      body:Center(
+      appBar: AppBar(title: Text('BMI RESULT '),),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Gender : ${isMale! ? 'Male' : 'female' }',style: TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold),),
-            Text('Result : ${result}',style: TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold),),
-            Text('AGE : ${age}',style: TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold),),
+            Text('Result : ${r(result!)}',style: TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold),),
+            Text('AGE : ${age!}',style: TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold),),
           ],
         ),
-      ) ,
+      ),
     );
   }
 }
+
