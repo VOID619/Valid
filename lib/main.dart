c9250ba47eb8c01b04fb7ef2099fa1133be9d3ab
Mohamed Ibrahim <92342092+VOID619@users.noreply.github.com>
2021-10-26 16:33:36 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
-import'package:flutter/material.dart';
-import 'package:project12/modules/bmi/BMI_CAculator.dart';
+import 'package:flutter/material.dart';
 
-
-
+import 'LoginS.dart';
 void main()
 {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home:BMI());
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
   }
 }
